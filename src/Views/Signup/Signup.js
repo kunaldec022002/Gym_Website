c72765c66b7aa36cb06103fc8c5c2ba8ffcc6223
kunaldec022002 <kunalingale72@gmail.com>
2023-11-29 14:29:58 +0530

    signup page completed

--- src/Views/Signup/Signup.js
+++ src/Views/Signup/Signup.js
@@ -1,14 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import './Signup.css'
+import Navbar from "../../Components/Navbar/Navbar";
+
 
 function Signup(){
 
-    return(
 
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+    
+        // Basic form validation
+        if (!formData.username || !formData.email || !formData.password) {
+          alert('All fields are required');
+          return;
+        }
+    
+        // Check if the user already exists
+        const users = JSON.parse(localStorage.getItem('users')) || [];
+        const existingUser = users.find((user) => user.email === formData.email);
+        if (existingUser) {
+          alert('User with this email already exists');
+          return;
+        }
+    
+        // Save the user data to local storage
+        users.push(formData);
+        localStorage.setItem('users', JSON.stringify(users));
+    
+        // Reset the form
+        setFormData({
+          username: '',
+          email: '',
+          password: '',
+        });
+    
+        alert('Signup successful!');
+        window.location.href ="/login"
+      };
+    
+      return (
         <div>
-            <h1 className="text-center">Signup</h1>
-        </div>
-    )
-}
+            <Navbar/>
+                    <div className="signup-container">
+                    <h2 className="text-center">Signup</h2>
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                        Username:
+                        <input
+                            className="input-text"
+                            type="text"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleChange}
+                            required
+                        />
+                        </label>
+                        <br />
+                        <label>
+                        Email:
+                        <input
+                            className="input-text"
+                            type="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
+                        </label>
+                        <br />
+                        <label>
+                        Password:
+                        <input
+                            className="input-text"
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            required
+                        />
+                        </label>
+                        <br />
+                        <button type="submit" className="signup-btn">Signup</button>
+                    </form>
+                    </div>
+        </div>            
+      );
+    };
+    
 
 export default Signup;
